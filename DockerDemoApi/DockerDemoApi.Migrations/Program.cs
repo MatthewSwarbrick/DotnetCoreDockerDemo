270f3a5e57090c7518d79de3d417bb849a5cd3f1
Matthew Swarbrick <matthew.swarbrick@rsmuk.com>
2018-03-06 09:50:14 +0000

    add migrations project and update connection string in test project

---
+++ DockerDemoApi/DockerDemoApi.Migrations/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using DbUp;
+
+namespace DockerDemoApi.Migrations
+{
+    class MainClass
+    {
+        public static int Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("You need to pass in the connection string");
+                return -1;
+            }
+
+            var connectionString = args.FirstOrDefault();
+
+            var upgrader =
+                DeployChanges.To
+                    .SqlDatabase(connectionString)
+                    .WithScriptsEmbeddedInAssembly(Assembly.GetExecutingAssembly())
+                    .WithTransactionPerScript()
+                    .LogToConsole()
+                    .Build();
+
+            var result = upgrader.PerformUpgrade();
+
+            if (!result.Successful)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(result.Error);
+                Console.ResetColor();
+#if DEBUG
+                Console.ReadLine();
+#endif
+                return -1;
+            }
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Success!");
+            Console.ResetColor();
+            return 0;
+        }
+    }
+}
