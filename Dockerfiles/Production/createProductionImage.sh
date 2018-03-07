e1aa19b3be45478ab31a6445f5e6b353d953eee2
Matthew Swarbrick <matthew.swarbrick@rsmuk.com>
2018-03-07 11:23:51 +0000

    fixed create production image script

--- Dockerfiles/Production/createProductionImage.sh
+++ Dockerfiles/Production/createProductionImage.sh
@@ -7,8 +7,8 @@ cd ../../DockerDemoApi
 echo Publishing docker demo api
 dotnet clean
 dotnet build
-dotnet publish -o ../../Dockerfiles/Production/publish
-cd ..\Dockerfiles\Production
+dotnet publish -o ../Dockerfiles/Production/publish
+cd ../Dockerfiles/Production
 
 echo Building production image
 docker build -t mswarbrick/dockerdemoapi:prod .
